7357ab853a2ae769a9d1a21b5ae63ea361e824fe
Ahmed Amin <ahmedamin1700@gmail.com>
2022-03-07 15:06:39 +0200

    add env to eb automatic

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 cd www
-printenv > .env
+
 eb init --region $AWS_DEFAULT_REGION $EB_APP
+
+eb setenv URL=$URL
+eb setenv PORT=$PORT
+eb setenv JWT_SECRET=$JWT_SECRET
+
+eb setenv POSTGRES_HOST=$POSTGRES_HOST
+eb setenv POSTGRES_PORT=$POSTGRES_PORT
+eb setenv POSTGRES_USERNAME=$POSTGRES_USERNAME
+eb setenv POSTGRES_PASSWORD=$POSTGRES_PASSWORD
+eb setenv POSTGRES_DB=$POSTGRES_DB
+
 eb deploy $EB_ENV
